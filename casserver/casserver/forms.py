89931b799599e19c6f3e86d9801c888865c6dab8
jdelic <jonas-bitbucket@maurus.net>
2016-08-28 22:49:45 +0200

    add placeholders for user forms for Django admin

---
+++ casserver/casserver/forms.py
@@ -0,0 +1,12 @@
+# -* encoding: utf-8 *-
+from django.forms.models import ModelForm
+
+
+class MNUserCreationForm(ModelForm):
+    # TODO: placeholder
+    pass
+
+
+class MNUserChangeForm(ModelForm):
+    # TODO: placeholder
+    pass
