3d5b46bebe12927b985d19f9cea51f1fb91925cb
jdelic <jonas-bitbucket@maurus.net>
2016-08-29 11:14:10 +0200

    override base user forms for MNUser

--- casserver/casserver/forms.py
+++ casserver/casserver/forms.py
@@ -1,12 +1,18 @@
 # -* encoding: utf-8 *-
-from django.forms.models import ModelForm
+import django.contrib.auth.forms as auth_forms
+
+from casserver.models import MNUser
 
 
-class MNUserCreationForm(ModelForm):
-    # TODO: placeholder
-    pass
+class MNUserCreationForm(auth_forms.UserCreationForm):
+    class Meta:
+        model = MNUser
+        fields = ('identifier',)
+        field_classes = {'identifier': auth_forms.UsernameField}
 
 
-class MNUserChangeForm(ModelForm):
-    # TODO: placeholder
-    pass
+class MNUserChangeForm(auth_forms.UserChangeForm):
+    class Meta:
+        model = MNUser
+        fields = '__all__'
+        field_classes = {'identifier': auth_forms.UsernameField}
