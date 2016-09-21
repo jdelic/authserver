e15a2d3addc6099cbe4f1e1ac925d818543c27d5
jdelic <jonas-bitbucket@maurus.net>
2016-09-21 14:43:26 +0200

    add branding

--- authserver/authserver/settings.py
+++ authserver/authserver/settings.py
@@ -31,7 +31,7 @@ ROOT_URLCONF = 'authserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "authserver", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -146,3 +146,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "authserver", "static")]
