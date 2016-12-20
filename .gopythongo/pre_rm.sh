de85ff29b575c03bcf39b2353b42cf06b4324689
jdelic <jonas-bitbucket@maurus.net>
2016-12-20 01:58:52 +0100

    add pre_rm script

---
+++ .gopythongo/pre_rm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "remove" ]; then
+    find /usr/local/authserver/lib -name \*.pyc -delete
+    find /usr/local/authserver -name __pycache__ -print0 | xargs -0 rm -rf
+fi
