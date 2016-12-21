42c7b8fbccda550672c5f0d7d20e0b405cde46e2
jdelic <jonas-bitbucket@maurus.net>
2016-12-21 02:02:22 +0100

    modify pre_rm so it also removes the Django collected static files

--- .gopythongo/pre_rm.sh
+++ .gopythongo/pre_rm.sh
@@ -2,4 +2,5 @@
 
 # fpm wraps this script so it is executed inside a shell function, so we know that $1 == "remove" here
 find /usr/local/authserver/lib -name \*.pyc -delete
-find /usr/local/authserver -name __pycache__ -print0 | xargs -0 rm -rf
+find /usr/local/authserver -type d -name __pycache__ -print0 | xargs -0 rm -rf
+find /usr/local/authserver -type d -name static -print0 | xargs -0 rm -rf
