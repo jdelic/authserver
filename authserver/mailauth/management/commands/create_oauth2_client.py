15396eb8ca96e30bb81960e296b7d57427b45b44
Jonas Maurus <jonas-bitbucket@maurus.net>
2017-04-08 19:08:55 +0200

    start of implementation for management commands to create, list and delete OAuth2 clients

---
+++ authserver/mailauth/management/commands/create_oauth2_client.py
@@ -0,0 +1,83 @@
+# -* encoding: utf-8 *-
+import argparse
+import json
+
+import os
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from oauth2_provider import settings as oauth2_settings, models as oauth2_models
+from typing import Any
+
+
+appmodel = oauth2_settings.APPLICATION_MODEL  # type: oauth2_models.Application
+
+
+class Command(BaseCommand):
+    requires_migrations_checks = True
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument_group("Config output options")
+        parser.add_argument("--write-to-stdout", dest="write_to_stdout", action="store_true", default=False,
+                            help="write the OAuth2 credentials to stdout as a JSON object.")
+        parser.add_argument("--write-to-consulkv", dest="write_to_consulkv", default=None,
+                            help="write the OAuth2 credentials to Hashicorp Consul. The credentials will be stored in "
+                                 "key/value pairs under the specified path.")
+        parser.add_argument("--write-to-vault", dest="write_to_vault", default=None,
+                            help="write the OAuth2 credentials to Hashicorp Vault. The credentials will be stored in "
+                                 "key/value paris under the specified path. The path must reside in a Vault 'secret' "
+                                 "backend. Set environment variables as specified by '12factor-vault' to configure "
+                                 "Vault authentication in the Authserver settings.")
+        parser.add_argument("--consul-url", dest="consul_url",
+                            default=os.getenv("CONSUL_ADDR", "http://127.0.0.1:8500/"),
+                            help="URL to use to contact the local Consul agent. Will use $CONSUL_ADDR from the "
+                                 "environment if it exists.")
+        parser.add_argument("--consul-token", dest="consul_token",
+                            default=None,
+                            help="An optional Consul ACL token. Will use the value of $CONSUL_TOKEN from the "
+                                 "environment if it exists.")
+
+        parser.add_argument_group("OAuth2 options")
+        parser.add_argument("--skip-authorization", dest="skip_authorization", default=False, action="store_true",
+                            help="Skip user authorization screen when connecting this application. Only use this "
+                                 "for internal applications and when you know what you're doing!")
+        parser.add_argument("--redirect-uri", dest="redirect_uris", default=[], action="append",
+                            help="Valid redirect URIs for this OAuth2 client. Can be specified multiple times.")
+        parser.add_argument("--client-type", dest="client_type", default="confidential",
+                            choices=["public", "confidential"],
+                            help="Choose the OAuth2 client type between 'public' and 'confidential'")
+        parser.add_argument("--grant-type", dest="grant_type", default=appmodel.GRANT_AUTHORIZATION_CODE,
+                            choices=[appmodel.GRANT_AUTHORIZATION_CODE, appmodel.GRANT_IMPLICIT,
+                                     appmodel.GRANT_CLIENT_CREDENTIALS, appmodel.GRANT_PASSWORD],
+                            help="Choose the OAuth2 grant type for this client.")
+
+        parser.add_argument("client_name", nargs="?",
+                            help="A human-readable name for the OAuth2 client that can be used to rerieve the same "
+                                 "credentials later using this command.")
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        client = None
+        try:
+            client = appmodel.objects.get(name=options["client_name"])
+        except appmodel.DoesNotExist:
+            client = appmodel.objects.create(
+                name=options["client_name"],
+                redirect_uris="\n".join(options["redirect_uris"]),
+                skip_authorization=options["skip_authorization"],
+                authorization_grant_type=options["grant_type"],
+            )
+
+        if options["write_to_stdout"]:
+            self.stdout.write(json.dumps({
+                "client_id": client.client_id,
+                "client_secret": client.client_secret,
+            }, indent=4))
+
+        if options["write_to_consul"]:
+            pass
+
+        if options["write_to_vault"]:
+            pass
+
+        self.stderr.write(self.style.SUCCESS("Created client %s (ID: %s)") % (options["client_name"],
+                                                                              client.client_id))
