b1015b9dce51313d5119a447d0a448336c2b65e2
jdelic <jonas-bitbucket@maurus.net>
2017-07-16 21:01:03 +0200

    add subparser infrastructure for dockerauth commands

--- authserver/dockerauth/management/commands/dockerauth.py
+++ authserver/dockerauth/management/commands/dockerauth.py
@@ -1,9 +1,30 @@
 # -* encoding: utf-8 *-
-import argparse
+from argparse import ArgumentParser
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 
 
-class DockerAuthCommand(BaseCommand):
-    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
-        gr_docker = parser.add_argument_group("Docker Auth Commands")
+class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser) -> None:
+
+        cmd = self
+
+        class SubCommandParser(CommandParser):
+            def __init__(self, **kwargs) -> None:
+                super().__init__(cmd, **kwargs)
+
+        subparsers = parser.add_subparsers(
+            dest='type',
+            title="subcommands",
+            parser_class=SubCommandParser
+        )  # type: ArgumentParser
+
+        registry_parser = subparsers.add_parser("registry", help="Manage Docker Registry access")
+        repo_parser = subparsers.add_parser("repo", help="Manage Docker Repository access")
+
+        reg_subparser = registry_parser.add_subparsers(title="Registry access", parser_class=SubCommandParser)
+        reg_add_parser = reg_subparser.add_parser("add", help="Add a Docker Registry")
+        reg_remove_parser = reg_subparser.add_parser("remove", help="Remove a Docker Registry")
+
+    def handle(self, *args, **options) -> None:
+        pass
