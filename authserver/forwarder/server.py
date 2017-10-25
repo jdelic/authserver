bd0c04bc57f9d3bfa7fbeb48c54f8123f77d5a27
jdelic <jonas-bitbucket@maurus.net>
2017-10-25 22:13:19 +0200

    better SMTP error handling by catching smtplib exceptions

--- authserver/forwarder/server.py
+++ authserver/forwarder/server.py
@@ -22,6 +22,33 @@ _log = logging.getLogger(__name__)
 pool = None  # type: Pool
 
 
+def smtp_sendmail_wrapper(smtpobj: smtplib.SMTP, *args: Any, **kwargs: Any) -> Union[str, None]:
+    """
+    Wraps smtp.sendmail and handles all the exceptions it can throw
+    :param smtpobj: the smtplib.SMTP object to wrap
+    :param args: will be passed to .sendmail
+    :param kwargs: will be passed to .sendmail
+    :return: a SMTP return string or None
+    """
+    try:
+        smtpobj.sendmail(*args, **kwargs)
+    except smtplib.SMTPSenderRefused as e:
+        _log.info("Downstream server refused sender: %s (%s %s)", e.sender, e.smtp_code, e.smtp_error)
+        return "%s %s" % (e.smtp_code, e.smtp_error)
+    except smtplib.SMTPResponseException as e:
+        # This exception baseclass is for all exceptions that have a SMTP response code.
+        # Return the downstream error code upstream
+        _log.info("Unexpected response from server (passed upstream): %s %s", e.smtp_code, e.smtp_error)
+        return "%s %s" % (e.smtp_code, e.smtp_error)
+    except smtplib.SMTPRecipientsRefused as e:
+        _log.info("Some recipients where refused by the downstream server: %s", " ".join(e.recipients))
+        # TODO: find good error code
+    except smtplib.SMTPServerDisconnected as e:
+        _log.info("Downstream server unexpectedly disconnected: %s", str(e))
+        return "421 Possible network problem. Please try again."
+    return None
+
+
 class ForwarderSMTPChannel(SMTPChannel):
     def handle_error(self) -> None:
         # handle exceptions through asyncore. Using this implementation will make it go
@@ -76,7 +103,7 @@ class ForwarderServer(SMTPServer):
                         _log.info("%sForwarding email from <%s> to <%s> to domain @%s",
                                   "(Retry) " if "retry" in kwargs and kwargs["retry"] else "",
                                   mailfrom, rcptto, domain.redirect_to)
-                        smtp.sendmail(mailfrom, new_rcptto, data)
+                        smtp_sendmail_wrapper(smtp, mailfrom, new_rcptto, data)
                     continue
 
             # follow the same path like the stored procedure authserver_resolve_alias(...)
@@ -120,7 +147,7 @@ class ForwarderServer(SMTPServer):
                     _log.info("%sForwarding email from <%s> with new sender <%s> to <%s>",
                               "(Retry) " if "retry" in kwargs and kwargs["retry"] else "",
                               mailfrom, _newmf, alias.forward_to.addresses)
-                    smtp.sendmail(_newmf, alias.forward_to.addresses, data)
+                    smtp_sendmail_wrapper(smtp, _newmf, alias.forward_to.addresses, data)
 
         # if there are any remaining non-list/non-forward recipients, we inject them back to OpenSMTPD here
         if len(remaining_rcpttos) > 0:
@@ -128,7 +155,7 @@ class ForwarderServer(SMTPServer):
                 _log.info("%sReinjecting email from <%s> to remaining recipients <%s>",
                           "(Retry) " if "retry" in kwargs and kwargs["retry"] else "",
                           mailfrom, remaining_rcpttos)
-                smtp.sendmail(mailfrom, remaining_rcpttos, data)
+                smtp_sendmail_wrapper(smtp, mailfrom, remaining_rcpttos, data)
 
         _log.debug("Done processing.")
         return None
