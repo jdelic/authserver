6a1d628361595349dcd6575035c52ca480e3227d
jdelic <jonas-github@maurus.net>
2019-06-04 00:19:03 +0200

    switch DKIM to relaxed/relaxed canonicalization which is the recommended way that is more robust in transit

--- authserver/maildaemons/dkimsigner/server.py
+++ authserver/maildaemons/dkimsigner/server.py
@@ -55,7 +55,8 @@ class DKIMSignerServer(SaneSMTPServer):
         signed = False
         if dom is not None and dom.dkimkey:
             sig = dkim.sign(data, dom.dkimselector.encode("utf-8"), dom.name.encode("utf-8"),
-                            dom.dkimkey.replace("\r\n", "\n").encode("utf-8"))
+                            dom.dkimkey.replace("\r\n", "\n").encode("utf-8"),
+                            canonicalize=('relaxed', 'relaxed'))
             data = b"%s%s" % (sig, data)
             try:
                 logstr = data.decode('utf-8')
