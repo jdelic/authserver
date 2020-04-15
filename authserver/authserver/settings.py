582cd05176db70ea042d99b853af782f1fba934d
jdelic <jonas-github@maurus.net>
2020-04-15 23:51:26 +0200

    fix mypy analysis, which includes actual bugs. I'm impressed

--- authserver/authserver/settings.py
+++ authserver/authserver/settings.py
@@ -107,19 +107,17 @@ if DATABASES["default"]["ENGINE"] == 'django.db.backends.postgresql' or \
         DATABASES["default"]["OPTIONS"]["sslmode"] = "verify-full"
         DATABASES["default"]["OPTIONS"]["sslrootcert"] = os.getenv("POSTGRESQL_CA")
 
-    if os.getenv("DB_SSLCERT", None) and not (VaultAuth12Factor.has_envconfig() and
-                                                  os.getenv("VAULT_DATABASE_PATH", None)):
+    if os.getenv("DB_SSLCERT", None) and not \
+            (VaultAuth12Factor.has_envconfig() and os.getenv("VAULT_DATABASE_PATH", None)):
         DATABASES["default"]["OPTIONS"]["sslcert"] = os.getenv("DB_SSLCERT")
         DATABASES["default"]["OPTIONS"]["sslkey"] = os.getenv("DB_SSLKEY")
 
 if DEBUG:
-    ALLOWED_HOSTS = ['*',]  # type: List[str]
+    ALLOWED_HOSTS = ['*']  # type: List[str]
     CORS_ORIGIN_ALLOW_ALL = True
 else:
-    CORS_ORIGIN_WHITELIST = os.getenv("CORS_ORIGIN_WHITELIST").split(',') if \
-        os.getenv("CORS_ORIGIN_WHITELIST", False) else []
-    CORS_ORIGIN_REGEX_WHITELIST = os.getenv("CORS_ORIGIN_REGEX_WHITELIST").split(',') if \
-        os.getenv("CORS_ORIGIN_REGEX_WHITELIST", False) else []
+    CORS_ORIGIN_WHITELIST = os.getenv("CORS_ORIGIN_WHITELIST", "").split(',')
+    CORS_ORIGIN_REGEX_WHITELIST = os.getenv("CORS_ORIGIN_REGEX_WHITELIST", "").split(',')
 
 DOCKERAUTH_ALLOW_UNCONFIGURED_REPOS = django12factor.getenv_bool("DOCKERAUTH_ALLOW_UNCONFIGURED_REPOS")
 
