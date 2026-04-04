b2960d79731f440d8300ca8b7799337ed8388af3
Jonas Maurus <jonas-github@maurus.net>
2026-04-04 15:29:04 +0200

    add support for Basic auth to checkpassword view

--- authserver/mailauth/views.py
+++ authserver/mailauth/views.py
@@ -1,9 +1,11 @@
+import base64
 import json
 import logging
 from datetime import datetime
 from typing import Any, List, NamedTuple, cast
 from zoneinfo import ZoneInfo
 
+import binascii
 from django.contrib.auth import authenticate
 from django.http import HttpResponseBadRequest
 from django.http.request import HttpRequest
@@ -137,14 +139,51 @@ class UserLoginAPIView(JWTViewHelperMixin, View):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
+    def _parse_basic_auth(self, request: HttpRequest) -> "_AuthRequest | None":
+        auth_header = request.headers.get("Authorization")
+        if not auth_header:
+            return None
+
+        try:
+            scheme, encoded = auth_header.split(" ", 1)
+        except ValueError:
+            raise InvalidAuthRequest()
+
+        if scheme.lower() != "basic":
+            return None
+
+        try:
+            decoded = base64.b64decode(encoded.strip()).decode("utf-8")
+        except (binascii.Error, UnicodeDecodeError):
+            raise InvalidAuthRequest()
+
+        if ":" not in decoded:
+            raise InvalidAuthRequest()
+
+        username, password = decoded.split(":", 1)
+
+        if not username:
+            raise InvalidAuthRequest()
+
+        scopes: List[str] = []
+        scopes_header = request.headers.get("X-Auth-Scopes")
+        if scopes_header:
+            scopes = [s.strip() for s in scopes_header.split(",") if s.strip()]
+
+        return _AuthRequest(
+            username=username,
+            password=password,
+            scopes=list(set(scopes)),
+        )
+
     def _parse_request(self, request: HttpRequest) -> _AuthRequest:
         scopes = []  # type: List[str]
         if request.content_type == "application/json":
-            data = json.loads(request.body.decode('utf-8'))
+            data = json.loads(request.body.decode("utf-8"))
             if "username" not in data:
                 raise InvalidAuthRequest()
-            username = data['username']
-            password = data['password'] if "password" in data else None
+            username = data["username"]
+            password = data["password"] if "password" in data else None
             if "scopes" in data and isinstance(data["scopes"], list):
                 scopes = data["scopes"]
         else:
@@ -152,7 +191,7 @@ class UserLoginAPIView(JWTViewHelperMixin, View):
                 raise InvalidAuthRequest()
             username = request.POST["username"]
             password = request.POST["password"]
-            scopes = request.POST["scopes"].split(",")
+            scopes = request.POST["scopes"].split(",") if "scopes" in request.POST else []
 
         return _AuthRequest(
             username=username,
@@ -167,21 +206,42 @@ class UserLoginAPIView(JWTViewHelperMixin, View):
     @method_decorator(ratelimit(key='ip', rate='20/m', method="POST", block=True))
     def post(self, request: HttpRequest) -> HttpResponse:
         if not request.is_secure():
-            return HttpResponseBadRequest('{"error": "This endpoint must be called securely"}',
-                                          content_type="application/json")
+            return HttpResponseBadRequest(
+                '{"error": "This endpoint must be called securely"}',
+                content_type="application/json",
+            )
 
         try:
-            req_domain = Domain.objects.find_parent_domain(request.get_host(), require_jwt_subdomains_set=True)
+            req_domain = Domain.objects.find_parent_domain(
+                request.get_host(),
+                require_jwt_subdomains_set=True,
+            )
         except Domain.DoesNotExist:
-            return HttpResponseBadRequest('{"error": "Not a valid authorization domain"}',
-                                          content_type="application/json")
+            return HttpResponseBadRequest(
+                '{"error": "Not a valid authorization domain"}',
+                content_type="application/json",
+            )
 
-        try:
-            userdesc = self._parse_request(request)
-        except json.JSONDecodeError:
-            return HttpResponseBadRequest('{"error": "Invalid JSON"}', content_type="application/json")
-        except InvalidAuthRequest:
-            return HttpResponseBadRequest('{"error": "Missing parameters"}', content_type="application/json")
+        userdesc = self._parse_basic_auth(request)
+        if userdesc is None:
+            try:
+                userdesc = self._parse_request(request)
+            except json.JSONDecodeError:
+                return HttpResponseBadRequest(
+                    '{"error": "Invalid JSON"}',
+                    content_type="application/json",
+                )
+            except InvalidAuthRequest:
+                return HttpResponseBadRequest(
+                    '{"error": "Missing or invalid parameters"}',
+                    content_type="application/json",
+                )
+
+        if userdesc is None:
+            return HttpResponseBadRequest(
+                '{"error": "Unknown authorization error"}',
+                content_type="application/json",
+            )
 
         if userdesc.password:
             user = cast(MNUser, authenticate(username=userdesc.username, password=userdesc.password))
@@ -195,35 +255,46 @@ class UserLoginAPIView(JWTViewHelperMixin, View):
 
         if user is None:
             return HttpResponse(
-                '{"authenticated": false, "authorized": false}', content_type="application/json", status=401,
+                '{"authenticated": false, "authorized": false}',
+                content_type="application/json",
+                status=401,
             )
         elif user.delivery_mailbox is None:
             return HttpResponse(
-                '{"authenticated": false, "authorized": false}', content_type="application/json", status=401,
+                '{"authenticated": false, "authorized": false}',
+                content_type="application/json",
+                status=401,
             )
         elif userdesc.scopes and not user.has_app_permissions(userdesc.scopes):
             return HttpResponse(
                 '{"authenticated": %s, "authorized": false, "requested": "[%s]"}' %
                 ("true" if authenticated else "false", user.get_all_app_permission_strings()),
-                content_type="application/json", status=401,
+                content_type="application/json",
+                status=403,
             )
         else:
-            # user is possibly authenticated and authorized
-            jwtstr = self._create_jwt(claim={
-                "sub": userdesc.username,
-                "canonical_username": "%s@%s" % (user.delivery_mailbox.mailprefix, user.delivery_mailbox.domain.name),
-                "authenticated": authenticated,
-                "authorized": user.has_app_permissions(userdesc.scopes or set()),
-                "scopes": list(user.get_all_app_permission_strings()),
-                "nbf": int(datetime.timestamp(datetime.now(tz=ZoneInfo("UTC")))) - 5,
-                "exp": int(datetime.timestamp(datetime.now(tz=ZoneInfo("UTC")))) + 3600,
-                "iss": req_domain.name,
-                "aud": "net.maurus.authclient",
-            }, key_pemstr=req_domain.jwtkey)
+            jwtstr = self._create_jwt(
+                claim={
+                    "sub": userdesc.username,
+                    "canonical_username": "%s@%s" % (
+                        user.delivery_mailbox.mailprefix,
+                        user.delivery_mailbox.domain.name,
+                    ),
+                    "authenticated": authenticated,
+                    "authorized": user.has_app_permissions(userdesc.scopes or set()),
+                    "scopes": list(user.get_all_app_permission_strings()),
+                    "nbf": int(datetime.timestamp(datetime.now(tz=ZoneInfo("UTC")))) - 5,
+                    "exp": int(datetime.timestamp(datetime.now(tz=ZoneInfo("UTC")))) + 3600,
+                    "iss": req_domain.name,
+                    "aud": "net.maurus.authclient",
+                },
+                key_pemstr=req_domain.jwtkey,
+            )
 
             return HttpResponse(
                 jwtstr,
-                content_type="application/jwt", status=200
+                content_type="application/jwt",
+                status=200,
             )
 
 
